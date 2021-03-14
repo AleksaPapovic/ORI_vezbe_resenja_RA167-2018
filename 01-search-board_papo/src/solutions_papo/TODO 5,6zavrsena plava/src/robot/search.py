0282308b8e5cf2cc993ae2151677237ae61ec4f1
AleksaPapovic <73531961+AleksaPapovic@users.noreply.github.com>
2021-03-14 22:49:17 +0100

    Add files via upload
    Dodata resenja prvih vezbi Aleksa Papovic RA167/2018

---
+++ 01-search-board_papo/src/solutions_papo/TODO 5,6zavrsena plava/src/robot/search.py
@@ -0,0 +1,235 @@
+from __future__ import print_function
+
+from collections import deque
+from abc import *
+import sys
+
+class Search(object):
+    """
+    Apstraktna klasa za pretragu.
+    """
+
+    def __init__(self, board):
+        self.board = board
+
+    def search(self, initial_state):
+
+        initial_state = initial_state(self.board)  # pocetno stanje
+        states_list = deque([initial_state])  # deque - "brza" lista u Python-u
+        states_set = {initial_state.unique_hash()}  # set - za brzu pretragu stanja
+
+        processed_list = deque([])  # deque procesiranih stanja
+        processed_set = set()  # set procesiranih stanja
+        slevel = 0
+
+        # pretraga
+        while len(states_list) > 0:  # dok ima stanja za obradu
+            curr_state = self.select_state(states_list)  # preuzmi sledece stanje za obradu
+
+
+            states_set.remove(curr_state.unique_hash())  # izbaci stanja iz seta stanja
+
+            processed_list.append(curr_state)  # ubaci stanje u listu procesiranih stanja
+            processed_set.add(curr_state.unique_hash())  # ubaci stanje u set procesiranih stanja
+
+            if curr_state.is_final_state():  # ako je krajnje stanje
+                # rekonsturisi putanju
+                return Search.reconstruct_path(curr_state), processed_list, states_list
+
+            # ako nije krajnje stanje
+            # izgenerisi sledeca moguca stanja
+            new_states = curr_state.get_next_states()
+            # iz liste sledecih mogucih stanja izbaci ona koja su vec u listi i koja su vec procesirana
+            new_states = [new_state for new_state in new_states if
+                          new_state.unique_hash() not in processed_set and
+                          new_state.unique_hash() not in states_set
+                          ]
+            # dodaj sledeca moguca stanja na kraj liste stanja
+            states_list.extend(new_states)
+            # dodaj sledeca moguca stanja u set stanja
+            states_set.update([new_state.unique_hash() for new_state in new_states])
+        return None, processed_list, states_list
+
+    @staticmethod
+    def reconstruct_path(final_state):
+        path = []
+        while final_state is not None:
+            path.append(final_state.position)
+            final_state = final_state.parent
+        return reversed(path)
+
+    @abstractmethod
+    def select_state(self, states):
+        """
+        Apstraktna metoda koja, na osnovu liste svih mogucih sledecih stanja,
+        bira sledece stanje za obradu.
+        *** STRATEGIJA PRETRAGE SE IMPLEMENTIRA OVERRIDE-ovanjem OVE METODE ***
+
+        :param states: lista svih mogucih sledecih stanja
+        :return: odabrano sledece stanje za obradu
+        """
+        pass
+
+
+class BreadthFirstSearch(Search):
+    def select_state(self, states):
+        # struktura podataka je red (queue)
+        # dodaj na kraj (linija 50), uzimaj sa pocetka
+        return states.popleft()
+
+
+class DepthFirstSearch(Search):
+    def select_state(self, states):
+        # TODO 1: Implementirati DFS
+        return states.pop()
+
+
+class IterativeDepthFirstSearch(Search):
+
+    def __init__(self, board, level, step):
+        super(self.__class__, self).__init__(board)
+        self.level = level
+        self.step = step
+
+    def search(self, initial_states):
+
+        start_depth = 0
+        processed_list_all = deque([])
+
+        for it in range(0,self.level,self.step):
+
+            initial_state = initial_states(self.board)  # pocetno stanje
+            states_list = deque([initial_state])  # deque - "brza" lista u Python-u
+            states_set = {initial_state.unique_hash()}  # set - za brzu pretragu stanja
+
+            processed_list = deque([])  # deque procesiranih stanja
+            processed_set = set()  # set procesiranih stanja
+
+            start_depth = start_depth + self.step
+            print("Dubina")
+            print(start_depth)
+            # pretraga
+
+            while len(states_list) > 0:  # dok ima stanja za obradu
+                curr_state = self.select_state(states_list,self.step,self.level)  # preuzmi sledece stanje za obradu
+                states_set.remove(curr_state.unique_hash())  # izbaci stanja iz seta stanja
+
+                processed_list.append(curr_state)  # ubaci stanje u listu procesiranih stanja
+                processed_set.add(curr_state.unique_hash())  # ubaci stanje u set procesiranih stanja
+                processed_list_all.append(curr_state)
+
+
+                if curr_state.is_final_state():  # ako je krajnje stanje
+                    # rekonsturisi putanju
+                    return Search.reconstruct_path(curr_state),  processed_list_all, states_list
+
+                # ako nije krajnje stanje
+                # izgenerisi sledeca moguca stanja
+                new_states = curr_state.get_next_states()
+                # iz liste sledecih mogucih stanja izbaci ona koja su vec u listi i koja su vec procesirana
+                new_states = [new_state for new_state in new_states if
+                              new_state.unique_hash() not in processed_set and
+                              new_state.unique_hash() not in states_set and
+                              new_state.get_current_cost() < start_depth
+                              ]
+                print(it)
+                # dodaj sledeca moguca stanja na kraj liste stanja
+
+                states_list.extend(new_states)
+
+                # dodaj sledeca moguca stanja u set stanja
+                states_set.update([new_state.unique_hash() for new_state in new_states])
+        return None,  processed_list_all, states_list
+
+    def select_state(self, states,step,level):
+        # TODO 2: Implementirati IDFS
+
+            return   states.pop()
+
+
+
+class GreedySearch(Search):
+    def select_state(self, states):
+        # TODO 3: Implementirati GS
+        # implementirati get_cost metodu u RobotState
+        min_cost = sys.float_info.max
+        min_state = None
+        for state in states:
+                cost = state.get_cost()
+                if min_cost > cost:
+                    min_state = state
+        states.remove(min_state)
+        return min_state
+
+
+class AStarSearch(Search):
+    def select_state(self, states):
+        # TODO 4: Implementirati A*
+        # implementirati get_cost i get_current_cost metode u RobotState
+        min_cost = sys.float_info.max
+        min_state = None
+        for state in states:
+            cost = state.get_cost() + state.get_current_cost()
+            if min_cost >cost:
+                min_cost = cost
+                min_state =state
+        states.remove(min_state)
+        return min_state
+
+    # TODO 5:
+class Nadjiplavu(Search):
+            def __init__(self, board, level, step):
+                super(self.__class__, self).__init__(board)
+                self.level = level
+                self.step = step
+
+            def search(self, initial_states):
+                initial_state = initial_states(self.board)  # pocetno stanje
+                states_list = deque([initial_state])  # deque - "brza" lista u Python-u
+                states_set = {initial_state.unique_hash()}  # set - za brzu pretragu stanja
+
+                processed_list = deque([])  # deque procesiranih stanja
+                processed_set = set()  # set procesiranih stanja
+                processed_list_all = deque([])  # deque procesiranih stanja
+                # pretraga
+                while len(states_list) > 0:  # dok ima stanja za obradu
+                    curr_state = self.select_state(states_list)  # preuzmi sledece stanje za obradu
+                    states_set.remove(curr_state.unique_hash())  # izbaci stanja iz seta stanja
+
+                    processed_list.append(curr_state)  # ubaci stanje u listu procesiranih stanja
+                    processed_set.add(curr_state.unique_hash())  # ubaci stanje u set procesiranih stanja
+                    processed_list_all.append(curr_state)
+                    if curr_state.is_pfood_state():
+
+                        initial_state = initial_states(self.board)  # pocetno stanje
+                        initial_state.change()
+                        states_list = deque([initial_state])  # deque - "brza" lista u Python-u
+                        states_set = {initial_state.unique_hash()}  # set - za brzu pretragu stanja
+
+                        processed_list = deque([])  # deque procesiranih stanja
+                        processed_set = set()  # set procesiranih stanja
+
+
+
+                    if curr_state.is_final_state() :  # ako je krajnje stanje
+
+                        return Search.reconstruct_path(curr_state),  processed_list_all, states_list
+                         #if curr_state.is_final_state():
+
+
+                    # ako nije krajnje stanje
+                    # izgenerisi sledeca moguca stanja
+                    new_states = curr_state.get_next_states()
+                    # iz liste sledecih mogucih stanja izbaci ona koja su vec u listi i koja su vec procesirana
+                    new_states = [new_state for new_state in new_states if
+                                  new_state.unique_hash() not in processed_set and
+                                  new_state.unique_hash() not in states_set]
+                    # dodaj sledeca moguca stanja na kraj liste stanja
+                    states_list.extend(new_states)
+                    # dodaj sledeca moguca stanja u set stanja
+                    states_set.update([new_state.unique_hash() for new_state in new_states])
+                return None,  processed_list_all, states_list
+
+            def select_state(self, states):
+
+                return states.popleft()
---
+++ 01-search-board_papo/src/solutions_papo/TODO 5,6zavrsena plava/src/robot/state.py
@@ -0,0 +1,198 @@
+from abc import *
+
+
+class State(object):
+    """
+    Apstraktna klasa koja opisuje stanje pretrage.
+    """
+
+    @abstractmethod
+    def __init__(self, board, parent=None, position=None, goal_position=None, food=None, old_goal_position=None):
+        """
+        :param board: Board (tabla)
+        :param parent: roditeljsko stanje
+        :param position: pozicija stanja
+        :param goal_position: pozicija krajnjeg stanja
+        :return:
+        """
+
+        self.board = board
+        self.parent = parent  # roditeljsko stanje
+        if self.parent is None:  # ako nema roditeljsko stanje, onda je ovo inicijalno stanje
+            self.position = board.find_position(self.get_agent_code())  # pronadji pocetnu poziciju
+            self.food = board.find_position(self.get_pfood_code())
+            self.goal_position = board.find_position(self.get_agent_goal_code())  # pronadji krajnju poziciju
+            self.old_goal_position = self.goal_position
+            self.goal_position = self.food
+
+        else:  # ako ima roditeljsko stanje, samo sacuvaj vrednosti parametara
+            self.position = position
+            self.food = food
+            self.goal_position = goal_position
+            self.old_goal_position = old_goal_position
+
+        self.depth = parent.depth + 1 if parent is not None else 1  # povecaj dubinu/nivo pretrage
+
+    def get_next_states(self):
+        new_positions = self.get_legal_positions()  # dobavi moguce (legalne) sledece pozicije iz trenutne pozicije
+        next_states = []
+        # napravi listu mogucih sledecih stanja na osnovu mogucih sledecih pozicija
+        for new_position in new_positions:
+            next_state = self.__class__(self.board, self, new_position, self.goal_position, self.food,
+                                        self.old_goal_position)
+            next_states.append(next_state)
+        return next_states
+
+    @abstractmethod
+    def get_agent_code(self):
+        """
+        Apstraktna metoda koja treba da vrati kod agenta na tabli.
+        :return: str
+        """
+        pass
+
+    @abstractmethod
+    def get_agent_goal_code(self):
+        """
+        Apstraktna metoda koja treba da vrati kod agentovog cilja na tabli.
+        :return: str
+        """
+        pass
+
+    @abstractmethod
+    def get_legal_positions(self):
+        """
+        Apstraktna metoda koja treba da vrati moguce (legalne) sledece pozicije na osnovu trenutne pozicije.
+        :return: list
+        """
+        pass
+
+    @abstractmethod
+    def is_final_state(self):
+        """
+        Apstraktna metoda koja treba da vrati da li je treuntno stanje zapravo zavrsno stanje.
+        :return: bool
+        """
+        pass
+
+    def is_pfood_state(self):
+        pass
+
+    @abstractmethod
+    def unique_hash(self):
+        """
+        Apstraktna metoda koja treba da vrati string koji je JEDINSTVEN za ovo stanje
+        (u odnosu na ostala stanja).
+        :return: str
+        """
+        pass
+
+    @abstractmethod
+    def get_cost(self):
+        """
+        Apstraktna metoda koja treba da vrati procenu cene
+        (vrednost heuristicke funkcije) za ovo stanje.
+        Koristi se za vodjene pretrage.
+        :return: float
+        """
+        pass
+
+    @abstractmethod
+    def get_current_cost(self):
+        """
+        Apstraktna metoda koja treba da vrati stvarnu trenutnu cenu za ovo stanje.
+        Koristi se za vodjene pretrage.
+        :return: float
+        """
+        pass
+
+    def get_pfood_code(self):
+        pass
+
+    def get_zfood_code(self):
+        pass
+
+    def change_goal(self):
+        pass
+
+
+class RobotState(State):
+    def __init__(self, board, parent=None, position=None, goal_position=None, food=None, old_goal_position=None):
+        super(self.__class__, self).__init__(board, parent, position, goal_position, food, old_goal_position)
+        # posle pozivanja super konstruktora, mogu se dodavati "custom" stvari vezani za stanje
+        # TODO 6: prosiriti stanje sa informacijom da li je robot pokupio kutiju
+
+    def get_agent_code(self):
+        return 'r'
+
+    def get_agent_goal_code(self):
+        return 'g'
+
+    def get_pfood_code(self):
+        return 'b'
+
+    def get_zfood__code(self):
+        return 'z'
+
+    def get_legal_positions(self):
+        # d_rows (delta rows), d_cols (delta columns)
+        # moguci smerovi kretanja robota (desno, levo, dole, gore)
+        # d_rows = [0, 0, 1, -1]
+        # d_cols = [1, -1, 0, 0]
+
+        d_rows = [0, 0, 1, -1, 1, -1, -1, 1]
+        d_cols = [1, -1, 0, 0, 1, 1, -1, -1]
+
+        row, col = self.position  # trenutno pozicija
+        new_positions = []
+        for d_row, d_col in zip(d_rows, d_cols):  # za sve moguce smerove
+            new_row = row + d_row  # nova pozicija po redu
+            new_col = col + d_col  # nova pozicija po koloni
+            # ako nova pozicija nije van table i ako nije zid ('w'), ubaci u listu legalnih pozicija
+            if 0 <= new_row < self.board.rows and 0 <= new_col < self.board.cols and self.board.data[new_row][
+                new_col] != 'w':
+                new_positions.append((new_row, new_col))
+        return new_positions
+
+    def is_final_state(self):
+        return self.position == self.goal_position
+
+    def is_pfood_state(self):
+        # if self.position == self.bfood_position:
+        print("pre izmene")
+        print(self.position)
+        print(self.food)
+        print(self.goal_position)
+        rp, cp = self.position
+        if (self.position == self.goal_position and self.board.data[rp][cp] == 'b'):
+            print("hrana")
+            print(self.food)
+            self.goal_position = self.old_goal_position
+            return True
+        return False
+
+    def change_goal(self):
+        self.goal_position = self.board.find_position(self.get_agent_goal_code())
+        print("izmena")
+        print(self.position)
+        print(self.food)
+
+    def unique_hash(self):
+        return str(self.position)
+
+    def get_current_cost(self):
+        return self.depth
+
+    def get_cost(self):
+        sr, sc = self.position
+        gr, gc = self.goal_position
+        return ((gr - sr) ** 2 + (gc - sc) ** 2) ** 0.5
+
+    def get_cost_m(self):
+        sr, sc = self.position
+        gr, gc = self.goal_position
+        return abs(gr - sr) + abs(gc - sc)
+
+    def change(self):
+        self.position = self.food
+        self.goal_position = self.old_goal_position
